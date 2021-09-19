079e7ce0f514a5f28fd66d4194e6aa8da4baeecd
Renan Garavelo <renan.garavelo@gmail.com>
2021-09-19 12:43:03 -0300

    Criado Carro.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None ):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
-        return f f'Olá {id(self)}'
+        return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Renan', 30)
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
